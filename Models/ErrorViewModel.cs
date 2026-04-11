20c4788fbda4e7b6a47a37f1ed686ff7eb09fdb6
Yocode5 <155065982+Yocode5@users.noreply.github.com>
2026-04-12 04:43:16 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Blindsync_PAS_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
