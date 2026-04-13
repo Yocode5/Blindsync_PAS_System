50cf8ba249ed8ca33ccfe921419fccfc0d8255fd
Yocode5 <155065982+Yocode5@users.noreply.github.com>
2026-04-13 23:53:01 +0530

    Add AdminController with views and fix ResearchAreas typo
    Added AdminController with [Authorize(Roles = "Admin")] and new actions for Overview, Users, and ResearchAreas. Updated ResearchAreas.cshtml with standard Razor structure and removed the incorrectly named ReseachAreas.cshtml file.

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,10 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Blindsync_PAS_System.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
         public IActionResult Overview()
+        {
+            ViewBag.UserName = User.Identity?.Name?.Split('@')[0] ?? "Guest";
+            return View();
+        }
+
+        public IActionResult Users()
+        {
+            return View();
+        }
+
+        public IActionResult ResearchAreas()
         {
             return View();
         }
