079014d7d2cb5b68d0f24984cc7e31b3c88cde37
Tharindu Dilshan <zahindilshan@gmail.com>
2026-04-14 21:33:26 +0530

    Implemented the UI of the Myproposals' Modal in Students

--- wwwroot/js/Student/MyProposals.js
+++ wwwroot/js/Student/MyProposals.js
@@ -1,1 +1,37 @@
-﻿
+﻿function openViewModal(title, area, tech, abstract) {
+    
+    document.getElementById('modalTitle').innerText = title;
+    document.getElementById('modalArea').innerText = area;
+    document.getElementById('modalAbstractText').innerText = abstract;
+
+    
+    const techContainer = document.getElementById('modalTechStack');
+    techContainer.innerHTML = ''; 
+
+    if (tech) {
+        tech.split(',').forEach(t => {
+            if (t.trim() !== "") {
+                const span = document.createElement('span');
+                span.className = 'tech-tag';
+                
+                span.innerHTML = `<span>${t.trim()}</span>`;
+                techContainer.appendChild(span);
+            }
+        });
+    }
+
+    
+    document.getElementById('viewAbstractModal').style.display = 'flex';
+}
+
+function closeViewModal() {
+    document.getElementById('viewAbstractModal').style.display = 'none';
+}
+
+
+function closeViewModalOutside(event) {
+    const overlay = document.getElementById('viewAbstractModal');
+    if (event.target === overlay) {
+        closeViewModal();
+    }
+}
