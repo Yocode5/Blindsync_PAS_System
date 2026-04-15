39f3a503b10c13e32ed8c3103466030bc237d82b
Yasith Jayanga <yasithjayanga221@gmail.com>
2026-04-15 15:10:38 +0530

    feat: add ReassignSupervisor functionality and DTO for project supervisor reassignment

---
+++ ViewModels/ReassignSupervisorDto.cs
@@ -0,0 +1,8 @@
+namespace Blindsync_PAS_System.ViewModels
+{
+    public class ReassignSupervisorDto
+    {
+        public int ProjectId { get; set; }
+        public int SupervisorId { get; set; }
+    }
+}
