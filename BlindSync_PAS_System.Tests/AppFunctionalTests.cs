3d02db4857932f23a9d3711a4065275d58497108
Yocode5 <155065982+Yocode5@users.noreply.github.com>
2026-04-17 21:59:26 +0530

    Add functional and unit tests; enable DbSet mocking
    Added AppFunctionalTests and AuthServiceMoqTests for functional and unit testing of authentication and login flows. Updated test project to reference Microsoft.AspNetCore.Mvc.Testing. Made Users DbSet virtual in AppDbContext to support mocking. Minor formatting adjustment in Program.cs.

---
+++ BlindSync_PAS_System.Tests/AppFunctionalTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+using Blindsync_PAS_System.Controllers;
+
+namespace BlindSync_PAS_System.Tests
+{
+    public class AppFunctionalTests : IClassFixture<WebApplicationFactory<AdminController>>
+    {
+        private readonly WebApplicationFactory<AdminController> _factory;
+
+        public AppFunctionalTests(WebApplicationFactory<AdminController> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task Get_LoginPage_LoadsSuccessfully()
+        {
+            // creating a fake browser
+            var client = _factory.CreateClient();
+
+            // navigate to the root (Home/Login)
+            var response = await client.GetAsync("/");
+
+            response.EnsureSuccessStatusCode();
+            Assert.Equal("text/html; charset=utf-8", response.Content.Headers.ContentType.ToString());
+        }
+
+        [Fact]
+        public async Task Get_SecureAdminPage_RedirectsToLogin_WhenUnauthenticated()
+        {
+            // creating a browser that doesn't follow redirects automatically
+            var client = _factory.CreateClient(new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false
+            });
+
+            // attempt to access the admin area without being logged in
+            var response = await client.GetAsync("/Admin/Overview");
+
+            // should try to redirect us (302) back to the login page
+            Assert.Equal(HttpStatusCode.Redirect, response.StatusCode);
+        }
+    }
+}
